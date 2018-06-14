7d8cbf3e325bc865d0e33d04b71bf8b0cce8aa9f
JulianTWolf <julianwolf@t-online.de>
2018-06-14 14:04:55 +0200

    13. commit

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,8 +6,6 @@ library(ggplot2)
 
 context("Test functions in the package")
 
-data_clean <- eq_clean_data(eq_location_clean(readr::read_delim("/Users/julian/Documents/07 Sonstiges/R-Projekte/RCapstone/signif.txt.tsv", col_names=T, delim = "\t")))
-
 test_that("geom_timeline returns a 'ggplot' object", {
   g <- data_clean %>% dplyr::filter(COUNTRY %in% c("GREECE", "ITALY"), YEAR > 2000) %>%
     ggplot2::ggplot(ggplot2::aes(x = DATE,
