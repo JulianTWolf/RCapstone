3e545060cc60256ad028964260445c90552d6119
JulianTWolf <julianwolf@t-online.de>
2018-06-25 11:39:42 +0200

    secound commit

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,8 @@
 library(testthat)
-library(RCapstone)
-library(tidyr)
-library(dplyr)
-library(ggplot2)
 
-context("Test functions in the package")
-
-system.file("extdata", "signif.txt.csv", package = "RCapstone")
+expect_that(geom_timeline_label(), throws_error())
+expect_that(eq_clean_data(), throws_error())
+expect_that(eq_create_label(), throws_error())
+expect_that(eq_location_clean(), throws_error())
+expect_that(eq_map(), throws_error())
+expect_that(geom_timeline(), throws_error())
